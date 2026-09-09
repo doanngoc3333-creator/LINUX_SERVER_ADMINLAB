b479e34617eb319a9b09142aab6f4f2f93543573
doanngoc3333 <doanngoc3333@gmail.com>
2026-09-09 19:26:10 +0700

    initial set up

---
+++ SetupHomelabServer.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install samba
+sudo apt install git
+sudo apt install -y curl wget vim nano htop unzip
+sudo apt install -y openssh-server avahi-daemon
+sudo apt install -y ufw fail2ban
+sudo apt install -y smartmontools nvme-cli parted gdisk rsync
