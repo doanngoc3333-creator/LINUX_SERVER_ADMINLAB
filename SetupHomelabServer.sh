ac20eba1efd1d6be22ac1bad07e0269828d0f386
doanngoc3333 <doanngoc3333@gmail.com>
2026-09-19 21:08:14 +0700

    update Setup

--- SetupHomelabServer.sh
+++ SetupHomelabServer.sh
@@ -6,3 +6,5 @@ sudo apt install -y curl wget vim nano htop unzip
 sudo apt install -y openssh-server avahi-daemon
 sudo apt install -y ufw fail2ban
 sudo apt install -y smartmontools nvme-cli parted gdisk rsync
+sudo apt install baobab
+sudo apt install -y nginx apache2 bind9 chrony
